d5670751fba5a00969743a50afb0aa6acd5067e9
Pavle Zikic <zikicpavle@gmail.com>
2022-10-06 14:42:46 +0200

    Updated the version of phpseclib

--- tests/RemoteSecLibTest.php
+++ tests/RemoteSecLibTest.php
@@ -2,9 +2,9 @@
 
 use Mockery as m;
 
-class RemoteSecLibGatewayTest extends PHPUnit_Framework_TestCase
+class RemoteSecLibGatewayTest extends \PHPUnit\Framework\TestCase
 {
-    public function tearDown()
+    public function tearDown(): void
     {
         m::close();
     }
